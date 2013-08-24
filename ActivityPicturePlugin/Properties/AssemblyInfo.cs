ea568795643b47bf7a17f15f63517005f65126e8
gerhard.nospam <gerhard.nospam@225033f3-47da-1d22-63f4-046357d733c6>
2013-08-24 23:15:51 +0000

    Bump for beta release

--- ActivityPicturePlugin/Properties/AssemblyInfo.cs
+++ ActivityPicturePlugin/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.5.67.*")]
-[assembly: AssemblyFileVersion("1.2")]
+[assembly: AssemblyVersion("2.0.168.*")]
+[assembly: AssemblyFileVersion("2.0")]
